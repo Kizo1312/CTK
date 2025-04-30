5007dd0d1eedc5f56561b909ce6e5caf2cf78d66
Kizo1312 <toma.ilcic@gmail.com>
2025-04-30 13:51:27 +0200

    dfgh

--- javascript/script.js
+++ javascript/script.js
@@ -10,16 +10,30 @@ function addCSS() {
 
   }
 }
-
-let unos = prompt("unesi ime")
+/*
+let unos = prompt("unesi text")
 let broj = unos.length
-let max = 140
+let max = 10
 if(!broj ){
 alert("alo, unesi nesto")
 }else if(broj > max){
-    alert("aloo previsee")
+    alert(unos.slice(0,max))
 }else {
-
-    let ostalo = max - broj
-    alert(broj +" ostalo "+ ostalo)
+    
+let ostalo = max - broj
+alert(unos +" ostalo "+ ostalo)
 }
+*/
+
+let unos = prompt("unesi broj godina")
+let max = 90
+let preostaleGodine = 90 - unos
+let mjeseci = preostaleGodine*12
+let dani = preostaleGodine*365
+if(unos > 90){
+    alert("ne valja")
+    
+}else{
+    alert(`do 90. rodendana vam preostaje ${preostaleGodine} godina ili ${mjeseci} mjeseci ili ${dani} dana`)
+}
+   
