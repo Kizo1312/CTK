172d8288203dae492a53f1a47ff944b5824d181c
Kizo1312 <toma.ilcic@gmail.com>
2025-05-19 16:21:05 +0200

    svasta

--- fdsfs/script.js
+++ fdsfs/script.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var firstName = "Toma";
 var yearOfBirth = 1996;
 function trenutnaDob(ime, dob) {
@@ -6,3 +21,121 @@ function trenutnaDob(ime, dob) {
 }
 console.log(trenutnaDob(firstName, yearOfBirth));
 console.log("hahaha");
+function add(a, b) {
+    return a + b;
+}
+var hobbies = ["hobi1", "hobi2", "hobi3"];
+hobbies.push("hobi4");
+var number = [1, 2, 3];
+var mixedArray;
+mixedArray = [1, "haha"];
+function proccesItems(items) {
+    var newItems = items.map(function (item) {
+        if (typeof (item) == "string") {
+            return item.toUpperCase();
+        }
+        else {
+            return item * item;
+        }
+    });
+    return newItems;
+}
+console.log(proccesItems(["toma", 5]));
+var nekiObj = {
+    name: "Ana",
+    age: 30,
+    akcije: ["nogomet"],
+    info: function () {
+        console.log("hahah");
+    }
+};
+nekiObj.name = "hhahaha";
+var obj = {};
+//lol
+console.log(nekiObj.info());
+//lol
+var student;
+function getActiveStudents(popis) {
+    var brojac = 0;
+    var aktivni = popis.map(function (student) {
+        if (student.isActive) {
+            return student.grade;
+        }
+        else {
+            brojac += 1;
+            return 0;
+        }
+    });
+    var sum = 0;
+    for (var i = 0; i < aktivni.length; i++) {
+        sum += aktivni[i];
+    }
+    return sum / (aktivni.length - brojac);
+}
+var studenti = [{ name: "toma", lastName: "ilcic", grade: 5, isActive: true },
+    { name: "toma", lastName: "ilcic", grade: 4, isActive: true },
+    { name: "toma", lastName: "ilcic", grade: 4, isActive: false }];
+console.log(getActiveStudents(studenti));
+var Role;
+(function (Role) {
+    Role[Role["Admin"] = 0] = "Admin";
+    Role[Role["Editor"] = 1] = "Editor";
+    Role[Role["Viewer"] = 2] = "Viewer";
+})(Role || (Role = {}));
+var user1 = {
+    name: "Toma",
+    role: Role.Admin
+};
+var user2 = {
+    name: "Matej",
+    role: Role.Editor
+};
+var user3 = {
+    name: "Andro",
+    role: Role.Viewer
+};
+var user4 = {
+    name: "Mihael",
+    role: Role.Admin
+};
+var users = [user1, user2, user3, user4];
+function getAdmins(useri) {
+    var admini = [];
+    useri.filter(function (user) {
+        if (user.role === Role.Admin) {
+            admini.push(user.name);
+        }
+    });
+    return admini;
+}
+console.log(getAdmins(users));
+var Korisnik = /** @class */ (function () {
+    function Korisnik(ime, godine) {
+        this.ime = ime;
+        this.godine = godine;
+    }
+    Korisnik.prototype.pozdravi = function () {
+        console.log("pozdrav od" + this.ime);
+    };
+    Korisnik.prototype.pokaziGodine = function () {
+        console.log(this.ime + " ima" + this.godine);
+    };
+    return Korisnik;
+}());
+var korisnik = new Korisnik("Toma", 29);
+korisnik.pozdravi();
+korisnik.pokaziGodine();
+var Admin = /** @class */ (function (_super) {
+    __extends(Admin, _super);
+    function Admin(ime, godine, specialKey) {
+        var _this = _super.call(this, ime, godine) || this;
+        _this.specialKey = specialKey;
+        return _this;
+    }
+    Admin.prototype.pokaziGodine = function () {
+        console.log("hahahah");
+    };
+    return Admin;
+}(Korisnik));
+var noviAdmin = new Admin("Toma", 29, true);
+noviAdmin.pokaziGodine();
--- fdsfs/script.ts
+++ fdsfs/script.ts
@@ -1,10 +1,183 @@
-var firstName: string = "Toma";
-var yearOfBirth: number = 1996;
+const firstName: string = "Toma";
+const yearOfBirth: number = 1996;
 
-function trenutnaDob2(ime: string, dob: number) {
+function trenutnaDob(ime: string, dob: number) {
     let currentAge: number = 2025 - dob;
     return "ime: " + ime + " Dob: " + currentAge;
 }
 
 console.log(trenutnaDob(firstName, yearOfBirth));
 console.log("hahaha");
+
+function add(a:number, b:number):number {
+    return a+b
+
+}
+let hobbies = ["hobi1", "hobi2", "hobi3"]
+hobbies.push("hobi4")
+let number:number[] =[1,2,3]
+let mixedArray:(string|number) [];
+mixedArray = [1,"haha"]
+
+
+function proccesItems(items:any[]){
+ const newItems =  items.map((item) =>{
+    if (typeof(item) == "string"){
+        return item.toUpperCase()
+    }else{
+        return item*item
+    }
+ })
+ return newItems
+}
+
+console.log(proccesItems(["toma", 5]))
+
+let nekiObj: {
+    name:string,
+    age: number,
+    akcije: string[],
+   info: ()=> void
+
+
+} = {
+    name:"Ana",
+    age:30,
+    akcije: ["nogomet"],
+    info: ()=>{
+        console.log("hahah")
+    }
+}
+nekiObj.name ="hhahaha"
+
+let obj = {};
+//lol
+console.log(nekiObj.info())
+//lol
+let student:{
+    name:string,
+    lastName:string,
+    grade:number,
+    isActive:boolean
+}
+
+function getActiveStudents(popis:{
+    name:string,
+    lastName:string,
+    grade:number,
+    isActive:boolean
+}[]){
+    let brojac:number = 0
+   const aktivni =popis.map((student) =>{
+    if (student.isActive){
+        return student.grade
+    }
+    else{
+        brojac +=1
+        return 0
+    }
+
+    })
+    let sum:number = 0
+    for(let i = 0; i < aktivni.length; i++){
+       
+
+            sum += aktivni[i]
+        
+
+    }
+    return sum / (aktivni.length - brojac)
+}
+const studenti:any[] = [{name:"toma", lastName:"ilcic", grade:5, isActive:true},
+                        {name:"toma", lastName:"ilcic", grade:4, isActive:true},
+                        {name:"toma", lastName:"ilcic", grade:4, isActive:false}]
+console.log(getActiveStudents(studenti))
+
+enum Role  {
+Admin,
+Editor,
+Viewer
+}
+
+
+const user1:{} = {
+    name: "Toma",
+    role: Role.Admin
+
+}
+const user2:{} = {
+    name:"Matej",
+    role: Role.Editor
+
+}
+const user3:{} = {
+    name:"Andro",
+    role: Role.Viewer
+
+}
+const user4:{} = {
+    name:"Mihael",
+    role: Role.Admin
+}
+const users:{}[] = [user1,user2,user3,user4]
+
+function getAdmins(useri:any[]){
+    let admini:string[] =[]
+     useri.filter((user) => {
+         if(user.role === Role.Admin){
+           admini.push (user.name)
+
+       }
+
+        
+    })
+    return admini
+   
+
+    
+}
+
+
+
+console.log(getAdmins(users))
+
+
+
+class Korisnik{
+    public ime:string;
+    private godine:number;
+
+    constructor(ime:string, godine:number){
+         this.ime=ime
+        this.godine =godine
+    }
+    
+        
+
+       
+    
+    public pozdravi():void{
+        console.log("pozdrav od" + this.ime)
+    }
+    public pokaziGodine():void{
+        console.log(this.ime + " ima" + this.godine)
+    }
+}
+
+const korisnik = new Korisnik("Toma", 29);
+korisnik.pozdravi();
+korisnik.pokaziGodine()
+
+class Admin extends Korisnik{
+    private specialKey: boolean;
+    constructor(ime:string, godine:number, specialKey:boolean){
+        super(ime,godine)
+        this.specialKey = specialKey
+    }
+    public pokaziGodine(): void {
+        console.log("hahahah")
+    }
+}
+const noviAdmin = new Admin("Toma", 29, true)
+
+noviAdmin.pokaziGodine()
