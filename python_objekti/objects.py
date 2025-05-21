e8a93cf0948d2c387f9ac74de300fcc7e4c17770
Kizo1312 <toma.ilcic@gmail.com>
2025-05-21 10:58:40 +0200

    store

--- python_objekti/objects.py
+++ python_objekti/objects.py
@@ -33,7 +33,31 @@ class Store:
              zbroj += item["price"]
          return zbroj
     def __str__(self):
-        return self.stock_price()
+        return f"Store: {self.name}, Items: {len(self.items)}, value: {self.stock_price()}"
+    @staticmethod
+    def franchise(store):
+        store.name= f"Franchise-{store.name}"
+        newStore = Store(store.name, store.items)
+        return newStore
+    @staticmethod
+    def compare_value(store1, store2):
+        if store1.stock_price() > store2.stock_price():
+            return store1.name
+        elif store2.stock_price() > store1.stock_price():
+            return store2.name
+        else:
+            return "They are the same"
+    @classmethod
+    def from_year(cls, year, items):
+        return cls(f"generated- {year}",items)
+    @classmethod
+    def from_item_list(cls,name, items_list):
+        return cls(name,items_list)
+
+    
+        
+
+    
     
          
              
@@ -42,10 +66,17 @@ class Store:
          
     
 konzum = Store("Konzum", [{"name":"banana","price":25}])
+plodine = Store("Plodine",[{"name":"jabuka", "price":35}])
+tommy = konzum.from_year(1996, konzum.items)
+noviD= konzum.from_item_list(konzum.name, [{"name":"jabuka", "price": 26}])
+print(noviD)
 
-print(konzum.name)      
+print(tommy)
 
-konzum.add_item("jabuka", 33)
-print(konzum.items)
-print(konzum.__str__())
 
+
+print(konzum)
+konzum.add_item("kiwi", 36)
+print(konzum.stock_price())
+print(Store.compare_value(konzum, plodine))
+print(Store.franchise(konzum))
